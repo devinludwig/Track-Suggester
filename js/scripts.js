036baf4c381d506f58c33b08428cb351a136b3ab
Devin Ludwig <devinludwig@gmail.com>
2016-10-21 10:56:27 -0700

    add content to all files to create a page with a name input and three dropdown menus that produce a recommended track when the submit button is pushed, with styling.

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+
+    $('#match , #ruby , #java , #php').hide();
+
+    var size = $('#size').val();
+    var variety = $('#variety').val();
+    var free = $('#free').val();
+    var nameInput = $("input#name").val();
+
+    $("#nameinput").text(nameInput);
+
+    if (size === 'large' && variety === 'one' && free === 'company') {
+      $('#match').show();
+       $("#java").show();
+    }
+
+    else if (size === 'small' && variety === 'one' && free === 'company') {
+      $('#match').show();
+       $("#ruby").show();
+    }
+
+    else if (disposition === 'anti' && activity === 'hyper') {
+      $('#match').show();
+       $("#grumpy").show();
+    }
+
+    else {
+      $('#match').show();
+       $("#evilqueen").show();
+    }
+  });
+});
