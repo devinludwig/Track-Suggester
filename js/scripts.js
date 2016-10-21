09c651f7c9b0d1e562188bd699f7c51a60e10217
Devin Ludwig <devinludwig@gmail.com>
2016-10-21 15:20:31 -0700

    add more branches and branch outcomes to javascript, including a Design track outcome.

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,12 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
-    $('#match , #ruby , #java , #php').hide();
+    $('#match , #ruby , #java , #php , #csharp , #design').hide();
 
     var size = $('#size').val();
     var variety = $('#variety').val();
     var free = $('#free').val();
+    var user = $('#user').val();
     var nameInput = $("input#name").val();
 
     $("#nameinput").text(nameInput);
@@ -14,21 +15,35 @@ $(document).ready(function() {
     if (size === 'large' && variety === 'one' && free === 'company') {
       $('#match').fadeIn();
        $("#java").show();
+       $('.or').show();
+       $("#csharp").show();
     }
 
     else if (size === 'small' && variety === 'one' && free === 'company') {
-      $('#match').show();
+      $('#match').fadeIn();
        $("#ruby").show();
     }
 
-    else if (disposition === 'anti' && activity === 'hyper') {
-      $('#match').show();
-       $("#grumpy").show();
+    else if (size === 'small' && variety === 'multiple' && free === 'company') {
+      $('#match').fadeIn();
+       $("#php").show();
+    }
+
+    else if (size === 'small' && variety === 'multiple' && free === 'freelance') {
+      $('#match').fadeIn();
+       $("#php").show();
+    }
+
+    else if (size === 'small' && variety === 'multiple' && free === 'freelance' && user === 'exp') {
+      $('#match').fadeIn();
+       $("#php").show();
+       $('.or').show();
+       $("#design").show();
     }
 
     else {
-      $('#match').show();
-       $("#evilqueen").show();
+      $('#match').fadeIn();
+       $("#ruby").show();
     }
   });
 });
